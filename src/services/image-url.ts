899f4682c989a4b86e3a3e58b2982ceb0e87e256
Lehaiwa Machaba <lehaiwa.software.dev@gmail.com>
2024-09-27 15:50:31 +0200

    Handle game data with no image url

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -9,10 +9,11 @@
  */
 
 import React from 'react';
+import NoImageUrl from '../assets/no-image-placeholder.webp';
 
 const getCroppedImageUrl = (url: string) => {
 
-    if (!url)  return '';
+    if (!url)  return NoImageUrl;
     
     const index = url.indexOf('media/') + 'media/'.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
